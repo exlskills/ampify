8a117211c3564f39d61e71dfcb4d34119c042133
Sasha Varlamov <sasha.varlamov@exlhub.com>
2018-09-30 18:45:50 +0800

    Add health check

--- routes/index.ts
+++ routes/index.ts
@@ -44,6 +44,7 @@ const gqlBaseControllerHandler = (promise: ControllerFunction, params: ParamsFun
 // Convenient short-hand version
 const gc = gqlBaseControllerHandler;
 
+router.get('/health-check', (req, res) => res.sendStatus(200))
 router.get('/amp/learn-:locale/courses/:course/units/:unit/sections/:section/card/:card', gc(getCourseSectionCard, req => [req.params.locale, fromUrlId('Course', req.params.course), fromUrlId('CourseUnit', req.params.unit), req.params.unit, fromUrlId('UnitSection', req.params.section), req.params.section, fromUrlId('SectionCard', req.params.card)]))
 
 export default router;
